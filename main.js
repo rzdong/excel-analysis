75eb8d8e6496dcb2782e5c17eebcb57b99347d31
zhendong ren <zhendong ren>
2019-10-31 11:17:54 +0800

    兼容处理。最后一行不存在报错。增加trycatch捕获

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const assets = 'file/';
 const files = fs.readdirSync(path.join(__dirname, assets))
 files.forEach((item, index) => {
 	let sheets = xlsx.parse(assets + item);
-	
+
 
 	for (let k = 0; k < sheets.length; k++) {
 
@@ -26,23 +26,28 @@ files.forEach((item, index) => {
 				let unit = sheets[k]['data'][i][j];
 
 				if (unit === 'NaN') {
+					try {
+						if (sheets[k]['data'][i - 1][j] !== 'NaN' && sheets[k]['data'][i + 1] && sheets[k]['data'][i + 1][j] !== 'NaN') {
+							sheets[k]['data'][i][j] = Number(((Number(sheets[k]['data'][i - 1][j]) + Number(sheets[k]['data'][i + 1][j])) / 2).toFixed(2));
+						} else if (sheets[k]['data'][i - 1][j] !== 'NaN' && sheets[k]['data'][i + 2] && sheets[k]['data'][i + 2][j] !== 'NaN') {
 
-					if (sheets[k]['data'][i - 1][j] !== 'NaN' && sheets[k]['data'][i + 1][j] !== 'NaN') {
-						sheets[k]['data'][i][j] = Number(((Number(sheets[k]['data'][i - 1][j]) + Number(sheets[k]['data'][i + 1][j])) / 2).toFixed(2));
-					} else if (sheets[k]['data'][i - 1][j] !== 'NaN' && sheets[k]['data'][i + 2][j] !== 'NaN') {
+							let delta = Number(((Number(sheets[k]['data'][i + 2][j]) - Number(sheets[k]['data'][i - 1][j])) / 3).toFixed(2));
+							sheets[k]['data'][i][j] = Number((Number(sheets[k]['data'][i - 1][j]) + delta).toFixed(2));
+							sheets[k]['data'][i + 1][j] = Number((Number(sheets[k]['data'][i - 1][j]) + delta * 2).toFixed(2));
+							// console.log(typeof delta, sheets[k]['data'][i][j], sheets[k]['data'][i + 1][j])
+						} else if (sheets[k]['data'][i - 1][j] !== 'NaN' && sheets[k]['data'][i + 3] && sheets[k]['data'][i + 3][j] !== 'NaN') {
+							let delta = Number(((Number(sheets[k]['data'][i + 3][j]) - Number(sheets[k]['data'][i - 1][j])) / 4).toFixed(2));
+							sheets[k]['data'][i][j] = Number((Number(sheets[k]['data'][i - 1][j]) + delta).toFixed(2));
+							sheets[k]['data'][i + 1][j] = Number((Number(sheets[k]['data'][i - 1][j]) + delta * 2).toFixed(2));
+							sheets[k]['data'][i + 2][j] = Number((Number(sheets[k]['data'][i - 1][j]) + delta * 3).toFixed(2));
+						} else {
 
-						let delta = Number(((Number(sheets[k]['data'][i + 2][j]) - Number(sheets[k]['data'][i - 1][j])) / 3).toFixed(2));
-						sheets[k]['data'][i][j] = Number((Number(sheets[k]['data'][i - 1][j]) + delta).toFixed(2));
-						sheets[k]['data'][i + 1][j] = Number((Number(sheets[k]['data'][i - 1][j]) + delta * 2).toFixed(2));
-						// console.log(typeof delta, sheets[k]['data'][i][j], sheets[k]['data'][i + 1][j])
-					} else if (sheets[k]['data'][i - 1][j] !== 'NaN' && sheets[k]['data'][i + 3][j] !== 'NaN') {
-						let delta = Number(((Number(sheets[k]['data'][i + 3][j]) - Number(sheets[k]['data'][i - 1][j])) / 4).toFixed(2));
-						sheets[k]['data'][i][j] = Number((Number(sheets[k]['data'][i - 1][j]) + delta).toFixed(2));
-						sheets[k]['data'][i + 1][j] = Number((Number(sheets[k]['data'][i - 1][j]) + delta * 2).toFixed(2));
-						sheets[k]['data'][i + 2][j] = Number((Number(sheets[k]['data'][i - 1][j]) + delta * 3).toFixed(2));
-					} else {
-
+						}
+					} catch (error) {
+						console.log(error);
+						console.log(`出错了！错误所在表${sheets[k]}表, 计算第${i+1}行, 第${j+1}列。`);
 					}
+
 
 				}
 
@@ -53,14 +58,14 @@ files.forEach((item, index) => {
 	var buffer = xlsx.build(sheets);
 
 
-	
 
-	fs.writeFile('target/'+item, buffer, function (err) {
+
+	fs.writeFile('target/' + item, buffer, function (err) {
 		if (err) {
 			console.log("Write failed: " + err);
 			return;
 		}
-		console.log("写入"+item+"完成");
+		console.log("写入" + item + "完成");
 	});
 })
 
